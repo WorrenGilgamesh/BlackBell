24e01c3e8ab4db617cfe3dc1653c49c00926bc97
sdist <sdist@itam.mx>
2017-06-28 13:39:08 -0500

    movimientos

--- challenges3.py
+++ challenges3.py
@@ -1,3 +1,4 @@
+#python /../../../BlackBell/challenges3.py
 import math
 def digitos(tam,esc):
 	len(str(123))
@@ -5,22 +6,43 @@ def digitos(tam,esc):
 	for x in esc:
 		t=t+(x,)
 	return t
-def creaEsc(tam,esc):
+def jugador(posicion):
+	return posicion
+def creaEsc(tam,esc,jugadorPos):
 	for x in esc:
 		if x%tam==0:
-			print('{:>4}'.format(x))
+			if jugador(jugadorPos)==x:
+				print('{:>4}'.format('a'))
+			else:
+				print('{:>4}'.format(x))
+			print(' ')
 		else:
-			print('{:>4}'.format(x), end=' ')
-
+			if jugador(jugadorPos)==x:
+				print('{:>4}'.format('a'), end=' ')
+			else:
+				print('{:>4}'.format(x), end=' ')
 def arreEsc(tam):
 	arre=[]
 	#for y in range(1,tam+1):
 	for x in range(1,tam+1):
 		arre.append(x)
 	return(arre)
+def pista(var,jugadorPos):
+	if var<=16:
+		var=var*var
+		creaEsc(var/math.sqrt(var),arreEsc(var),jugadorPos)
+		return True
+	else:
+		print("Un numero del 1 al 16")
+		return False
+def start():
+	print("comenzamos el juego elige el tamaño del escenario")
+	res=False
+	while (res ==False):
+		tam=int(input())
+		res=pista(tam,1)
+	while (res ==True):
+		print('Elige una pos')
+		pista(tam,int(input()))
 
-def juego(var):
-	var=var*var
-	creaEsc(var/math.sqrt(var),arreEsc(var))
-
-juego(45)
+start()
