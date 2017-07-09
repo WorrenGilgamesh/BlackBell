6b8d47e50083c519fa3081a4b0a001d69f7be267
Victor Osornio <victor.osornio@emqu.net>
2017-07-08 19:18:09 -0500

    algo

--- IA/test.py
+++ IA/test.py
@@ -1,21 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import codecs
+import time
+
+
 
 #Porcesador de lemario
 #procesador web para sacar palabra, significado, ejemplo, separacion
 #===================================================================================================
 #===================================================================================================
-def palabra():
-	with codecs.open('lemari.txt',encoding='utf-8') as fin:
-		for line in fin:
-			page = requests.get("http://acepciones.com/"+line+"/")
-			soup = BeautifulSoup(page.content, 'html.parser')
-			count=0
-			for x in soup.find_all('tr'):
-				print(soup.find_all('tr')[count].get_text())
-				count +=1
+def palabra(line):
+	sign=""
+	page = requests.get("http://acepciones.com/"+line+"/")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	count=0
+	for x in soup.find_all('tr'):
+		sign+=soup.find_all('tr')[count].get_text()
+		count +=1
+	return sign
 #===================================================================================================
 #===================================================================================================
 
-palabra()
+def diccionario():
+	with codecs.open('lemari.txt',encoding='utf-8') as fin:
+		for line in fin:			
+			print(palabra(line))
+
+diccionario()
